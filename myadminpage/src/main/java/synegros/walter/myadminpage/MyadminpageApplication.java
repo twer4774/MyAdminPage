3bfc43acd9a3eb933b6ffc0c0ea61781730694be
wonik <twer4774@gmail.com>
2021-03-21 18:05:05 +0900

    Init: "프로젝트 생성"

---
+++ myadminpage/src/main/java/synegros/walter/myadminpage/MyadminpageApplication.java
@@ -0,0 +1,13 @@
+package synegros.walter.myadminpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyadminpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyadminpageApplication.class, args);
+	}
+
+}
---
+++ myadminpage/src/test/java/synegros/walter/myadminpage/MyadminpageApplicationTests.java
@@ -0,0 +1,13 @@
+package synegros.walter.myadminpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyadminpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
